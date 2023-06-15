95592a9c26afbb5fc5172dc19f670dd0c234657d
nguyenminhkham <ng.minhkham@gmail.com>
2023-06-15 15:40:04 +0700

    test api 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
-use App\View\Components\Navbar;
-use App\View\Components\Courses;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        Blade::component('package-navbar', Navbar::class);
-        Blade::component('package-courses', Courses::class);
+        //
     }
 }
